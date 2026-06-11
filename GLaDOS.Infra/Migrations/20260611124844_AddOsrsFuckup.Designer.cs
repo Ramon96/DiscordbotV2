b79e09f1658a049ec99ada4fc55f149622fedfda
Ramon96 <rfm1@live.nl>
2026-06-11 14:55:43 +0200

    Add /osrsfuckup, /osrsfuckups, /osrscorrectfuckup commands with OsrsFuckup entity and migration (#12)
    Co-authored-by: GLaDOS Feature Bot <glados-bot@discord.bot>

--- GLaDOS.Infra/Migrations/20260611124844_AddOsrsFuckup.Designer.cs
+++ GLaDOS.Infra/Migrations/20260611124844_AddOsrsFuckup.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using GLaDOS.Infra.EntityFramework;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace GLaDOS.Infra.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260611124844_AddOsrsFuckup")]
+    partial class AddOsrsFuckup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
